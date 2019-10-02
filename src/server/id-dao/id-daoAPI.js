8a01f82861bd932be7b9bfa96e186b24ccdd8e8b
dOrgJelli <jelli@dorg.tech>
2019-10-02 09:26:38 -0400

    propose-add

--- src/server/id-dao/id-daoAPI.js
+++ src/server/id-dao/id-daoAPI.js
@@ -2,8 +2,11 @@
 import { Router } from 'express'
 import passport from 'passport'
 import { wrapAsync } from '../utils/helpers'
-import AdminWallet from '../blockchain/AdminWallet'
-import { isHuman, setWeb3Provider } from '@dorgtech/id-dao-client'
+import { isHuman, setWeb3Provider, proposeAdd } from '@dorgtech/id-dao-client'
+
+// TODO: make this middleware like gundb?
+import IPFSClient from 'ipfs-http-client'
+const ipfs = IPFSClient('127.0.0.1', process.env.ID_DAO_IPFS_PORT)
 
 const setup = (app: Router, storage: StorageAPI) => {
   // TODO: add "main-net" provider to AdminWallet
@@ -31,7 +34,7 @@ const setup = (app: Router, storage: StorageAPI) => {
       const address = body.address
 
       if (!address) {
-        throw new Error('Address not supplied')
+        throw new Error('address not supplied')
       }
 
       const result = await isHuman(address)
@@ -42,14 +45,61 @@ const setup = (app: Router, storage: StorageAPI) => {
     })
   )
 
-  /*app.post(
-    '/id-dao/propose-add'
+  /**
+   * @api {post} /id-dao/propose-add Propose human
+   * @apiName Propose human to DAO
+   * @apiGroup Identity DAO
+   *
+   * @apiParam {String} identityDefinition
+   * @apiParam {String} signature
+   *
+   * @apiSuccess {String} proposalId
+   * @ignore
+   */
+  app.post(
+    '/id-dao/propose-add',
+    passport.authenticate('jwt', { session: false }),
+    wrapAsync(async (req, res, next) => {
+      const { body, log } = req
 
-    identityDefinition
-    signature
+      log.debug('/id-dao/propose-add', 'proposing')
+      log.debug('/id-dao/propose-add', 'body: ', body)
+
+      if (!body.signature) {
+        throw new Error('signature not supplied')
+      }
+
+      if (!body.identityDefinition) {
+        throw new Error('identityDefinition not supplied')
+      }
+
+      const identity = JSON.parse(body.identityDefinition)
+      const signature = body.signature
+
+      const ipfsRes = await ipfs.add(Buffer.from(JSON.stringify(identity)))
+
+      if (ipfsRes.length === 0) {
+        const error = 'Error uploading to IPFS, no response'
+        log.error(error)
+
+        res.json({
+          ok: 1,
+          error
+        })
+      } else {
+        const hash = ipfsRes[0].path
+        const proposalId = await proposeAdd(identity.address, hash, signature, {
+          title: `GoodDollar: Add '${identity.name}'`
+        })
+
+        res.json({
+          proposalId
+        })
+      }
+    })
   )
 
-  app.post(
+  /*app.post(
     '/id-dao/propose-edit'
 
     identityDefinition
@@ -65,7 +115,6 @@ const setup = (app: Router, storage: StorageAPI) => {
   /*
 
 - [Identity DAO](#id-dao)
-	- [Propose add identity](#propose-add-id)
 	- [Propose edit identity](#propose-edit-id)
 	- [Proposal status](#proposal-status)
 
